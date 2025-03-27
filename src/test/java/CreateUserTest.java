333ffd8676d8297e2a42b962b6b001dcdb182bfd
Landrien <landemarrehadrien@gmail.com>
2025-03-27 16:51:15 +0100

    Automatisation de test API avec REST Assured

--- src/test/java/CreateUserTest.java
+++ src/test/java/CreateUserTest.java
@@ -1,4 +1,33 @@
-package PACKAGE_NAME;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.hamcrest.core.IsNull.notNullValue;
 
 public class CreateUserTest {
+@Test
+    public void testCreateUserTest(){
+        File jsonFile = new File("src/test/resources/create_user.json");
+
+      Response response =
+            given()
+                .baseUri("https://reqres.in")
+                .basePath("/api/users")
+                .contentType(ContentType.JSON)
+                .body(jsonFile)
+                .when()
+                .post()
+                .then()
+                .statusCode(201)
+                    .body("name", equalTo("morpheus"))
+                    .body("job", equalTo("leader"))
+                    .body("id", notNullValue())
+                .log().all()
+                .extract().response();
+    }
+
 }
--- src/test/java/GetUserTest.java
+++ src/test/java/GetUserTest.java
@@ -1,4 +1,24 @@
-package PACKAGE_NAME;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.hamcrest.core.StringContains.containsString;
 
 public class GetUserTest {
+
+    @Test
+    public void testGetUser(){
+        given().baseUri("https://reqres.in")
+                .basePath("/api/users/2")
+                .when()
+                .get()
+                .then()
+                .statusCode(200)
+                .body("data.id", equalTo(2))
+                .body("data.first_name", equalTo("Janet"))
+                .body("data.email", containsString("@reqres.in"))
+                .log().all();
+    }
+
+
 }
